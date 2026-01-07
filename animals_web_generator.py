48495f61bb2a55c75565a793c6c5b9a5aebdecdb
dovgusha1 <dovgusha1@gmail.com>
2026-01-07 23:45:44 +0100

    formated the html script

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -10,24 +10,26 @@ animals_string_data = ""
 
 for animal in animals_data:
     animals_string_data += '<li class="cards__item">\n'
-
+    animals_string_data += '<div class="card__title">\n'
     # Name
     if "name" in animal:
-        animals_string_data += f"Name: {animal['name']}<br/>\n"
+        animals_string_data += f"Name: {animal['name']}</div>\n"
 
+    animals_string_data += '<p class="card__text">\n'
     # Diet
     if "characteristics" in animal and "diet" in animal["characteristics"]:
-        animals_string_data += f"Diet: {animal['characteristics']['diet']}<br/>\n"
+        animals_string_data += f"<strong>Diet:</strong>: {animal['characteristics']['diet']}<br/>\n"
 
     # Locations (first one only)
     if "locations" in animal and animal["locations"]:
-        animals_string_data += f"Locations: {animal['locations'][0]}<br/>\n"
+        animals_string_data += f" <strong>Location:</strong> {animal['locations'][0]}<br/>\n"
 
     # Type
     if "characteristics" in animal and "type" in animal["characteristics"]:
-        animals_string_data += f"Type: {animal['characteristics']['type']}<br/>\n"
+        animals_string_data += f"<strong>Type:</strong> {animal['characteristics']['type']}<br/>\n"
 
-    animals_string_data += "</li>\n\n"
+    animals_string_data += "</p>\n"
+    animals_string_data += "</li>\n"
 
 with open("animals_template.html", "r", encoding="utf-8") as file:
     template_content = file.read()
